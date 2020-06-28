1088ab2b99cd605c9e2239e5079f62fa1aa13528
BG-YU <byunggun.yu@gmail.com>
2020-06-29 08:54:06 +0900

    Create 재귀함수_트리_경로출력.cpp

---
+++ 재귀함수 연습/재귀함수_트리_경로출력.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+char path[2];
+
+void run(int level){
+	if(level == 2) {
+		cout << path << endl;
+		return;
+	}
+	for(int i = 0; i < 3; i++){
+		path[level] = i + 'A';
+		run(level + 1);
+		path[level] = 0;
+	}
+}
+
+int main(){
+	run(0);
+	return 0;
+}
