62e7f2b87cb79b68ea6de302154e600469d89e38
BG-YU <byunggun.yu@gmail.com>
2020-06-29 10:31:34 +0900

    Create 재귀함수_트리_입력받은 문자 경로 탐색.cpp

---
+++ 재귀함수 연습/재귀함수_트리_입력받은 문자 경로 탐색.cpp
@@ -0,0 +1,33 @@
+﻿/*
+입력받은 3자리 문자가 몇번째 경로에서 호출되는지..
+*/
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+char arr[4];
+char path[4];
+int cnt = 0;
+
+void run(int level){
+	if(level == 3){
+		cnt++;
+		if(strcmp(path, arr) == 0){
+			cout << cnt << "번째";	
+		}
+		return;
+	}
+	
+	for(int i = 0; i < 4; i++){
+		path[level] = i + 'A';
+		run(level + 1);
+		path[level] = 0;
+	}
+}
+
+int main(){
+	cin >> arr;
+	run(0);
+	return 0;
+}
