61cd3fb3757d2a3d5cd1a2e46972c1d5590c132a
BG-YU <byunggun.yu@gmail.com>
2020-07-21 22:42:41 +0900

    Create 자물쇠와 열쇠.cpp

---
+++ 코딩테스트/2020 KAKAO BLIND RECRUITMENT/자물쇠와 열쇠.cpp
@@ -0,0 +1,32 @@
+﻿/*
+2020 KAKAO BLIND RECRUITMENT
+자물쇠와 열쇠
+https://programmers.co.kr/learn/courses/30/lessons/60059
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+    bool answer = true;
+
+    return answer;
+}
+
+int main() {
+    vector<vector<int>> key = {
+        {0,0,0},
+        {1,0,0},
+        {0,1,1}
+    };
+
+    vector<vector<int>> lock = {
+        {1,1,1},
+        {1,1,0},
+        {1,0,1}
+    };
+    solution(key, lock);
+    return 0;
+}
