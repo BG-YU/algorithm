8384ee7aeb902ab05d06759ac05d8e38bc9bfcf5
BG-YU <byunggun.yu@gmail.com>
2020-07-21 17:07:12 +0900

    Update 괄호 변환.cpp

--- 코딩테스트/2020 KAKAO BLIND RECRUITMENT/괄호 변환.cpp
+++ 코딩테스트/2020 KAKAO BLIND RECRUITMENT/괄호 변환.cpp
@@ -8,108 +8,63 @@ https://programmers.co.kr/learn/courses/30/lessons/60058
 #include <iostream>
 
 using namespace std;
+int index = 0;
 
+bool fn_chk(string p) {
+    vector<char> vect;
+    int cnt = 0, flag = 0;
 
-//vector<char> ret;
+    for (int i = 0; i < p.length(); i++) {
+        vect.push_back((char)(p[i]));
+        
+        if ((char)(p[i]) == '(') cnt++;
+        else cnt--;
 
-string ret = "";
+        if (cnt == 0 && flag == 0) {
+            index = i + 1;
+            flag = 1;
+        }
 
-int fn_chk(string p) {
-	vector<char> temp;
-	for (int i = 0; i < p.length(); i++) {
-		temp.push_back((char)(p[i]));
-		
-		if (temp.size() >= 2) {
-			if (temp[temp.size() - 1] == ')' && temp[temp.size() - 2] == '(') {
-				temp.pop_back();
-				temp.pop_back();
-			}
-		}
-	}
+        if (vect.size() >= 2) {
+            if (vect[vect.size() - 1] == ')' && vect[vect.size() - 2] == '(') {
+                vect.pop_back();
+                vect.pop_back();
+            }
+        }
+    }
 
-	return temp.size();
+    if (vect.size() == 0) return true;
+    else return false;
 }
 
-void run(string u, string v) {
-	int cnt = 0;
-	string str1 = "", str2 = "", temp = "";
-	int u_cnt = fn_chk(u);
-	if (u_cnt == 0) {
-		ret += u;
-		run(v, "");
-	}
-	else {
-		if (u_cnt == u.length()) {
-			int left = u.length() / 2 - 1;
-			int right = u.length() / 2;
+string solution(string p) {
+    if (p == "") return p;
+    if (fn_chk(p)) return p;
 
-			for (int i = 0; i < u.length() / 2; i++) {
-				if ((char)(u[left]) == '(' && (char)(u[right]) == ')') {
-					left--;
-					right++;
-				}
-				else {
-					u[left] = '(';
-					u[right] = ')';
-					left--;
-					right++;
-				}
-			}
-			temp += u;
-			temp += v;
-			ret += temp;
-			return;
-		}
-		else {
-			for (int i = 0; i < u.length(); i++) {
-				str1 += (char)(u[i]);
+    string answer = "", temp = "";
+    string u = "", v = "";
 
-				if ((char)(u[i]) == '(') cnt++;
-				else cnt--;
+    for (int i = 0; i < index; i++) u += (char)(p[i]);
+    for (int i = index; i < p.length(); i++) v += (char)(p[i]);
 
-				if (cnt == 0) {
-					for (int k = str1.length(); k < u.length(); k++) {
-						str2 += (char)(u[k]);
-					}
-					run(str1, str2);
-					break;
-				}
-			}
-		}
-	}
+    if (fn_chk(u)) return u + solution(v);
+    else {
+        for (int i = 1; i < u.length() - 1; i++) {
+            if ((char)(u[i]) == ')') u[i] = '(';
+            else u[i] = ')';
+            temp += (char)(u[i]);
+        }
+
+        answer = "(";
+        answer += temp;
+        answer += ")";
+        answer += v;
+    }
+    return answer;
 }
 
 int main() {
-	string p = ")(";
-	string answer = "";
-	string u = "", v = "";
-	int cnt = 0;
-
-	if (fn_chk(p) == 0 || p == "") {
-		cout << p;
-		return 0;
-	}
-
-	for (int i = 0; i < p.length(); i++) {
-		u += (char)(p[i]);
-		if ((char)(p[i]) == '(') cnt++;
-		else cnt--;
-		if (cnt == 0) {
-			if (fn_chk(u) == 0) {
-				for (int k = u.length(); k < p.length(); k++) {
-					v += (char)(p[k]);
-				}
-				run(u, v);
-				break;
-			}
-			else {
-				run(u, v);
-			}
-			
-		}
-	}
-
-	cout << ret;
-
-	return 0;
+    string str = "(()))(";
+    cout << solution(str);
+    return 0;
 }
