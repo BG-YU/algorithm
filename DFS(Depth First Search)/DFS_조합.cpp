414f5d27237defff4c3e19331b4f71b151d36c34
BG-YU <byunggun.yu@gmail.com>
2020-07-26 19:03:59 +0900

    Create DFS_조합.cpp

---
+++ DFS(Depth First Search)/DFS_조합.cpp
@@ -0,0 +1,29 @@
+﻿/*
+T,K,A,B 4사람이 3자리만 있는 놀이기구를 탈려고 한다.
+3자리로 구성할수 있는 조합을 출력하시오
+*/
+
+#include <iostream>
+using namespace std;
+
+char arr[5] = { "TKAB" };
+char path[3];
+
+void run(int lv, int start) {
+
+	if (lv == 3) {
+		cout << path << endl;
+		return;
+	}
+
+	for (int i = start; i < 4; i++) {
+		path[lv] = arr[i];
+		run(lv + 1, i + 1);
+		path[lv] = 0;
+	}
+}
+
+int main() {
+	run(0, 0);
+	return 0;
+}
