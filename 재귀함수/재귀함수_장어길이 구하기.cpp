44d5d31b2b7f7b87e2d8e80c56c7b75e230336a8
BG-YU <byunggun.yu@gmail.com>
2020-07-27 16:01:49 +0900

    Create 재귀함수_장어길이 구하기.cpp

---
+++ 재귀함수/재귀함수_장어길이 구하기.cpp
@@ -0,0 +1,40 @@
+﻿/*
+가장 긴 장어길이 구하기
+
+[입력]
+#~~~~~~~#~~~~#
+
+[출력]
+~~~~~~~
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int cnt, maxx = -21e8;
+string str;
+
+void run(int lv) {
+	if (str[lv] == '#') {
+		if (maxx < cnt) {
+			maxx = cnt;
+			cnt = 0;
+		}
+	}
+	else cnt++;
+
+	if (lv == str.length() - 1) return;
+
+	run(lv + 1);
+}
+
+int main() {
+	
+	cin >> str;
+	
+	run(0);
+
+	for (int i = 0; i < maxx; i++) cout << "~";
+
+	return 0;
+}
