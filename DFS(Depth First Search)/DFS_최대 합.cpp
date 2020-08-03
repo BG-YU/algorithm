31ce9cb7dd5832e1251678f70f3b8d3dc3f9833f
BG-YU <byunggun.yu@gmail.com>
2020-08-03 21:20:28 +0900

    Create DFS_최대 합.cpp

---
+++ DFS(Depth First Search)/DFS_최대 합.cpp
@@ -0,0 +1,41 @@
+﻿/*
+숫자 4개를 입력 받고 각각 *3,/2,-5를 했을 경우,
+가장 큰 합의 수는 얼마인가?
+*/
+
+#include <iostream>
+using namespace std;
+
+int arr[4];
+int maxx = -21e8;
+
+void dfs(int lv, int num) {
+	if (lv == 4) {
+		if (maxx < num) maxx = num;
+		return;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		switch (i) {
+		case 0:
+			dfs(lv + 1, num + (arr[lv] * 3));
+			break;
+		case 1:
+			dfs(lv + 1, num + (arr[lv] / 2));
+			break;
+		case 2:
+			dfs(lv + 1, num + (arr[lv] - 5));
+			break;
+		}
+	}
+}
+
+int main() {
+	for (int i = 0; i < 4; i++) {
+		cin >> arr[i];
+	}
+
+	dfs(0, 0);
+	cout << maxx;
+	return 0;
+}
