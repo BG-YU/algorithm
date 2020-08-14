be559e5fc5b407baf692e7266c7927f004f568f8
BG-YU <byunggun.yu@gmail.com>
2020-08-14 14:17:32 +0900

    Update GREEDY_동전갯수.cpp

--- GREEDY/GREEDY_동전갯수.cpp
+++ GREEDY/GREEDY_동전갯수.cpp
@@ -10,16 +10,50 @@ using namespace std;
 
 int main() {
 	int num, cnt = 0;
+	int arr[4] = { 500, 100, 50, 10 };
 	cin >> num;
+
 	while (1) {
-		if (num == 0) break;
-		else if (num >= 500) num -= 500;
-		else if (num >= 100) num -= 100;
-		else if (num >= 50) num -= 50;
-		else if (num >= 10) num -= 10;
+		if (num <= 0) break;
+
+		for (int i = 0; i < 4; i++) {
+			if (num >= arr[i]) {
+				num -= arr[i];
+				break;
+			}
+		}
 		cnt++;
 	}
 
 	cout << cnt;
 	return 0;
 }
+
+/*
+// [DFS 풀이]
+#include <iostream>
+using namespace std;
+
+int arr[4] = {500,100,50,10};
+int minn = 21e8;
+
+void dfs(int num, int cnt) {
+	if (num <= 0) {
+		if (minn > cnt) minn = cnt;
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (num < arr[i]) continue;
+		dfs(num - arr[i], cnt + 1);
+	}
+}
+
+int main() {
+	int num;
+	cin >> num;
+	dfs(num, 0);
+	cout << minn;
+	return 0;
+}
+*/
