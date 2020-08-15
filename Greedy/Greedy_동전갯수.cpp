1df529f9f5f24de5cca5ed788e9a8a5e4a9a46b3
BG-YU <byunggun.yu@gmail.com>
2020-08-15 20:40:51 +0900

    Create Greedy_동전갯수.cpp

---
+++ Greedy/Greedy_동전갯수.cpp
@@ -0,0 +1,59 @@
+﻿/*
+입력받은 거슴름돈을 돌려 줄대 최소의 동전 갯수는?
+
+동전
+500 100 50 10
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num, cnt = 0;
+	int arr[4] = { 500, 100, 50, 10 };
+	cin >> num;
+
+	while (1) {
+		if (num <= 0) break;
+
+		for (int i = 0; i < 4; i++) {
+			if (num >= arr[i]) {
+				num -= arr[i];
+				break;
+			}
+		}
+		cnt++;
+	}
+
+	cout << cnt;
+	return 0;
+}
+
+/*
+// [DFS 풀이]
+#include <iostream>
+using namespace std;
+
+int arr[4] = {500,100,50,10};
+int minn = 21e8;
+
+void dfs(int num, int cnt) {
+	if (num <= 0) {
+		if (minn > cnt) minn = cnt;
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (num < arr[i]) continue;
+		dfs(num - arr[i], cnt + 1);
+	}
+}
+
+int main() {
+	int num;
+	cin >> num;
+	dfs(num, 0);
+	cout << minn;
+	return 0;
+}
+*/
