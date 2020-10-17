586d286638933dc0794966e809dcf5be5df944ef
BG-YU <byunggun.yu@gmail.com>
2020-10-17 20:53:47 +0900

    Create Programmers_LV2_최댓값과 최솟값.cpp

---
+++ Programmers/연습문제/Programmers_LV2_최댓값과 최솟값.cpp
@@ -0,0 +1,50 @@
+﻿/*
+[문제 설명]
+문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 (최소값) (최대값)형태의 문자열을 반환하는 함수, solution을 완성하세요.
+예를들어 s가 1 2 3 4라면 1 4를 리턴하고, -1 -2 -3 -4라면 -4 -1을 리턴하면 됩니다.
+
+[제한 조건]
+s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+
+[입출력 예]
+s              return
+1 2 3 4	       1 4
+-1 -2 -3 -4	  -4 -1
+-1 -1         -1 -1
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    vector<int> vect;
+    int a = 0, b, c;
+    s.insert(s.length(), " ");
+
+    while (1) {
+        b = s.find(" ", a);
+        if (b < 0) break;
+
+        string tmp = s.substr(a, b - a);
+        vect.push_back(stoi(tmp));
+
+        a = b + 1;
+    }
+
+    sort(vect.begin(), vect.end());
+
+    answer += to_string(vect[0]);
+    answer += " ";
+    answer += to_string(vect.back());
+
+    return answer;
+}
+
+int main() {
+    solution("-1 -2 -3 -4");
+    return 0;
+}
